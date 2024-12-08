b849d5cf042e4cec526b3d4060201c708e4b55a8
Daniel <Da_919@hotmail.com>
2024-12-08 01:03:11 -0500

    feat: add .env file for environment variables and refactor config.php to load JWT settings from .env

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
-define('JWT_SECRET_KEY', 'clave_secreta_super_segura'); 
-define('JWT_EXPIRATION', 3600); 
+
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('JWT_SECRET_KEY', $_ENV['JWT_SECRET_KEY']);
+define('JWT_EXPIRATION', $_ENV['JWT_EXPIRATION']);
+define('API_URL', $_ENV['API_URL']);
