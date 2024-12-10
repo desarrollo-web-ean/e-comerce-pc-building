5f9ec577f42c9fe72a99ee11485a7416c848d4a7
Daniel <Da_919@hotmail.com>
2024-12-09 21:13:11 -0500

    fix: update header links to point to local project URLs for improved navigation

--- partials/header.php
+++ partials/header.php
@@ -15,12 +15,12 @@
       class="sticky top-0 z-40 shadow-md w-full backdrop-blur flex-none bg-white lg:flex lg:justify-between lg:items-center lg:px-36 h-16">
       <div class="">
         <span class="text-2xl">
-          <a href="/">Arma tu computadora</a>
+          <a href="http://localhost/proyectos/armatucomputadora/">Arma tu computadora</a>
         </span>
       </div>
       <div class="flex gap-4 items-center">
         <a href="#" class="text-xl hover:text-sky-700">About Us</a>
         <a href="#" class="text-xl hover:text-sky-700">Services</a>
-        <a href="#" class="text-xl hover:text-sky-700">Products</a>
+        <a href="http://localhost/proyectos/armatucomputadora/php/views/products/index.php" class="text-xl hover:text-sky-700">Products</a>
       </div>
     </header>
